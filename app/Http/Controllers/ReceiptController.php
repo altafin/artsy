4ad9cc7ddcf532656a57a93ae21dea32c82e058c
altafin <marbr72@hotmail.com>
2025-01-03 15:03:09 -0300

    Nova rota para gravar recibo e controle para validação.

--- app/Http/Controllers/ReceiptController.php
+++ app/Http/Controllers/ReceiptController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ReceiptRequest;
 use App\Models\Receipt;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class ReceiptController extends Controller
 {
@@ -12,6 +14,14 @@ class ReceiptController extends Controller
     {
         return response()->json([
             'data' => Receipt::paginate(5)
-        ]);
+        ], Response::HTTP_CREATED);
+    }
+
+    public function store(ReceiptRequest $request): JsonResponse
+    {
+        $receipt = Receipt::create($request->all());
+        return response()->json([
+            'data' => $receipt
+        ], Response::HTTP_CREATED);
     }
 }
